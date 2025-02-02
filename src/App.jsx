67c14a1531c074c22e514997eb63719c20c8d95d
Peter <youngeunpc@yahoo.com>
2025-02-02 13:08:18 -0300

    Finished project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 import Die from "./components/Die"
 import { nanoid } from 'nanoid'
 import Confetti from 'react-confetti'
 
 function App() {
   const [dice, setDice] = useState(() => generateAllNewDice())
+  const buttonRef = useRef(null)
   const gameWon = dice.every(die => die.isHeld) && dice.every(die => die.value === dice[0].value)
 
+  // Focus the New Game button when the game is won, better for keyboard users
+  useEffect(() => {
+    if (gameWon) {
+      buttonRef.current.focus()
+    }
+  }, [gameWon])
+
+  // Generate 10 new dice with random values
   function generateAllNewDice() {
     return new Array(10)
       .fill(0)
@@ -17,18 +26,25 @@ function App() {
       }))
   }
 
+  // Roll all dice that are not held
   function rollDice() {
     setDice(oldDice => oldDice.map(die =>
       die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }
     ))
+
+    if (gameWon) {
+      setDice(generateAllNewDice())
+    }
   }
 
+  // Toggle whether a die is held
   function hold(id) {
     setDice(oldDice => oldDice.map(die => 
         die.id === id ? { ...die, isHeld: !die.isHeld } : die
       ))
   }
 
+  // Create a Die component for each die object
   const diceElements = dice.map(dieObject => (
     <Die 
       key={dieObject.id} 
@@ -40,16 +56,19 @@ function App() {
 
   return (
     <main>
-      {gameWon && <Confetti />}
+      {gameWon && <Confetti className="m-auto mt-0 h-screen w-screen"/>}
+      <div aria-live="polite" className="sr-only">
+        {gameWon && <p>Congratulations, you won! Press New Game to start again.</p>}
+      </div>
       <h1 className="title">Tenzies</h1>
       <p className="instructions">
-        Roll until all dice are the same. Click each die to freeze it at its current value
-        between rolls.
+        {gameWon ? "Congratulations, you won!" : "Click each die to hold its value. Match all 10 to win."}
       </p>
       <div className="dice-container">
         {diceElements}
       </div>
       <button 
+        ref={buttonRef}
         className="roll-dice"
         onClick={rollDice}
       >
